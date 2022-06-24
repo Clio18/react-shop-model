176f6b7f19104a83e0c2d81d3883dd97bd175acc
U0L9511 <anton.obolonyk@esternibisp.com>
2022-06-24 16:50:01 +0300

    Add error handling during the fetching data

--- src/components/Meals/AvaliableMeals.js
+++ src/components/Meals/AvaliableMeals.js
@@ -6,36 +6,52 @@ import react, { useEffect, useState } from "react";
 const AvaliableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         "https://react-shop-model-default-rtdb.firebaseio.com/meals.json"
       );
+
+      if (!response.ok) {
+        throw new Error("Something goes wrong!");
+      }
+
       const data = await response.json();
 
       const loadedMeals = [];
-      for (const key in data){
+      for (const key in data) {
         loadedMeals.push({
           id: key,
           name: data[key].name,
           description: data[key].description,
-          price: data[key].price
-        })
+          price: data[key].price,
+        });
       }
       setMeals(loadedMeals);
       setIsLoading(false);
     };
 
-    fetchMeals();
+      fetchMeals().catch((error)=>{
+         setIsLoading(false);
+      setHttpError(error.message);
+      });
+
   }, []);
 
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
 
-  if (isLoading){
-    return <section className ={classes.MealsLoading}>
-      <p>Loading...</p>
-    </section>
+  if (httpError){
+    return(<section className={classes.MealsError}>
+      <p>{httpError}</p>
+    </section>)
   }
 
   const mealList = meals.map((meal) => (
